a0cd3163a1c87a858d29a858264413c257e0f167
Kendall Murphey <kendallmurphey@gmail.com>
2022-11-06 09:26:43 -0600

    added hat throwing and catching

--- Assets/Hat/Hat.cs
+++ Assets/Hat/Hat.cs
@@ -5,20 +5,60 @@ using UnityEngine;
 public class Hat : MonoBehaviour {
     [SerializeField]
     private GameObject _player;
-
     [SerializeField]
-    private Vector3 _offset;
-
+    private Vector3 _offsetOnPlayer;
+    [SerializeField]
+    private float _throwSpeed = 15f;
     [SerializeField]
     private bool _isOnPlayer = true;
 
-    private void Start() {
+    private bool _inAir = false;
 
+    private Rigidbody2D _rigidBody;
+    private Collider2D _collider;
+
+    private void Start() {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
+        SetPhysics(!_isOnPlayer);
     }
 
     private void Update() {
         if (_isOnPlayer) {
-            transform.position = _player.transform.position + _offset;
+            transform.position = _player.transform.position + _offsetOnPlayer;
         }
-    }    
+    }
+
+    public void Catch() {
+        _inAir = false;
+        _isOnPlayer = true;
+        SetPhysics(false);
+    }
+
+    public void Throw(Vector3 at) {
+        if (_isOnPlayer) {
+            _isOnPlayer = false;
+            SetPhysics(true);
+            _rigidBody.velocity = at * _throwSpeed;
+        }
+    }
+
+    private void SetPhysics(bool enabled) {
+        _collider.enabled = enabled;
+        _rigidBody.simulated = enabled;
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            if (!_isOnPlayer) {
+                _inAir = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Player") && _inAir) {
+            Catch();
+        }
+    }
 }
--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace DropOfAHat.Player.Input {
-    public class PlayerInput : MonoBehaviour {
+namespace DropOfAHat.Player {
+    public class Player : MonoBehaviour {
         [SerializeField]
         private bool _isEnabled = true;
 
         [SerializeField]
         private float _moveSpeed = 15f;
 
+        private Hat _hat;
         private Vector2 _moveInput;
         private Rigidbody2D _rigidBody;
+        private Camera _mainCam;
 
         private void Start() {
+            _mainCam = FindObjectOfType<Camera>();
             _rigidBody = GetComponent<Rigidbody2D>();
+            _hat = GetComponentInChildren<Hat>();
         }
 
         private void OnMove(InputValue input) =>
@@ -31,6 +35,15 @@ namespace DropOfAHat.Player.Input {
                 _rigidBody.velocity.y,
                 0f);
             _rigidBody.velocity = newVel;
+        }
+    
+        private void OnThrow() {
+            var mousePos = _mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            var throwVec = new Vector3(
+                mousePos.x - _hat.transform.position.x,
+                mousePos.y - _hat.transform.position.y,
+                0f).normalized;
+            _hat.Throw(throwVec);
         }
     }
 }
