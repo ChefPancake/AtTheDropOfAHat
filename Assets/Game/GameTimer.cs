c1ce2033b02f3af9a39d4803c99b5958d2dfa1f0
Kendall Murphey <kendallmurphey@gmail.com>
2022-11-08 21:16:02 -0600

    added main menu

--- Assets/Game/GameTimer.cs
+++ Assets/Game/GameTimer.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class GameTimer : MonoBehaviour {
-    private float _elapsedTime = 0;
-    
-    private string TimeString => $"{_elapsedTime:00.000}";
-    
-    private TextMeshProUGUI _text;
+namespace DropOfAHat.Game {
+    public class GameTimer : MonoBehaviour {
+        private float _elapsedTime = 0;
 
-    private void Start() {
-        _text = GetComponent<TextMeshProUGUI>();
-    }
+        private string TimeString => $"{_elapsedTime:00.000}";
 
-    private void Update() {
-        _elapsedTime += Time.deltaTime;
-        _text.text = TimeString;
+        private TextMeshProUGUI _text;
+
+        private void Start() {
+            _text = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update() {
+            _elapsedTime += Time.deltaTime;
+            _text.text = TimeString;
+        }
     }
 }
---
+++ Assets/MainMenu/MenuManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DropOfAHat.Menu {
+    public class MenuManager : MonoBehaviour {
+        [SerializeField]
+        private string _loadSceneOnPlay;
+
+        public void Play() {
+            SceneManager.LoadScene(_loadSceneOnPlay);
+        }
+
+        public void Quit() {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+            Application.Quit();
+        }
+    }
+}
