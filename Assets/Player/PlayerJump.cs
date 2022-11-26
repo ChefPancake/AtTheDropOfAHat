f779dd7b64ffa5d755db2e7d07851be3141d4688
Kendall Murphey <kendallmurphey@gmail.com>
2022-11-26 09:12:12 -0600

    added jump animation

--- Assets/Player/PlayerJump.cs
+++ Assets/Player/PlayerJump.cs
@@ -4,6 +4,9 @@ using UnityEngine.InputSystem;
 
 namespace DropOfAHat.Player {
     public class PlayerJump : MonoBehaviour {
+        private const string IS_IN_AIR_ANIMATION_STATE = "IsInAir";
+        private const string Y_VEL_ANIMATION_STATE = "YVel";
+
         [SerializeField]
         private float _jumpSpeed = 10f;
         [SerializeField]
@@ -18,10 +21,12 @@ namespace DropOfAHat.Player {
 
         private Rigidbody2D _rigidBody;
         private GameEvents _events;
+        private Animator _animator;
 
         void Start() {
             _rigidBody = GetComponent<Rigidbody2D>();
             _events = FindObjectOfType<GameEvents>();
+            _animator = GetComponentInChildren<Animator>();
             if (_floatLengthSeconds == 0f) {
                 throw new ArgumentException($"{nameof(_floatLengthSeconds)} cannot be 0");
             }
@@ -43,6 +48,7 @@ namespace DropOfAHat.Player {
                     _rigidBody.AddForce(new Vector2(0f, lerpFloatForce));
                 }
             }
+            _animator.SetFloat(Y_VEL_ANIMATION_STATE, _rigidBody.velocity.y);
         }
 
         private void OnJump(InputValue input) =>
@@ -57,12 +63,14 @@ namespace DropOfAHat.Player {
         private void OnCollisionEnter2D(Collision2D other) {
             if (other.gameObject.CompareTag("World")) {
                 _isGrounded = true;
+                _animator.SetBool(IS_IN_AIR_ANIMATION_STATE, false);
             }
         }
 
         private void OnCollisionExit2D(Collision2D other) {
             if (other.gameObject.CompareTag("World")) {
                 _isGrounded = false;
+                _animator.SetBool(IS_IN_AIR_ANIMATION_STATE, true);
             }
         }
     }
