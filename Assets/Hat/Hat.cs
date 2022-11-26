1988bf42f23100e1a5a25e6854797307d4039012
Kendall Murphey <kendallmurphey@gmail.com>
2022-11-26 16:18:11 -0600

    added animation to hat throw

--- Assets/Hat/Hat.cs
+++ Assets/Hat/Hat.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace DropOfAHat.Hat {
     public class Hat : MonoBehaviour {
+        private const string IS_ON_PLAYER_ANIMATION_STATE = "IsOnPlayer";
+        private const string VELOCITY_ANIMATION_STATE = "Velocity";
+
         [SerializeField]
         private GameObject _player;
         [SerializeField]
@@ -15,11 +18,13 @@ namespace DropOfAHat.Hat {
         private Rigidbody2D _rigidBody;
         private Collider2D _collider;
         private GameEvents _events;
+        private Animator _animator;
 
         private void Start() {
             _events = FindObjectOfType<GameEvents>();
             _rigidBody = GetComponent<Rigidbody2D>();
             _collider = GetComponent<Collider2D>();
+            _animator = GetComponentInChildren<Animator>();
             if (_isOnPlayer) {
                 Catch();
             }
@@ -30,12 +35,18 @@ namespace DropOfAHat.Hat {
         private void Update() {
             if (_isOnPlayer) {
                 transform.position = _player.transform.position + _offsetOnPlayer;
+            } else {
+                _animator.SetFloat(
+                    VELOCITY_ANIMATION_STATE, 
+                    _rigidBody.velocity.magnitude);
             }
         }
 
         public void Catch() {
             _inAir = false;
             _isOnPlayer = true;
+            _animator.SetBool(IS_ON_PLAYER_ANIMATION_STATE, true);
+            _animator.SetFloat(VELOCITY_ANIMATION_STATE, 0f);
             SetPhysics(false);
             _events.Send(new CaughtEvent());
         }
@@ -43,6 +54,7 @@ namespace DropOfAHat.Hat {
         private void OnThrown(PlayerThrow.HatThrown thrown) {
             if (_isOnPlayer) {
                 _isOnPlayer = false;
+                _animator.SetBool(IS_ON_PLAYER_ANIMATION_STATE, false);
                 SetPhysics(true);
                 _rigidBody.velocity = thrown.ThrowVec;
             }
