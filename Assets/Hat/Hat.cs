233cc71546889b09031e48afeb1914de54ba8673
Kendall Murphey <kendallmurphey@gmail.com>
2022-11-29 11:18:48 -0600

    refactored hat

--- Assets/Hat/Hat.cs
+++ Assets/Hat/Hat.cs
@@ -3,8 +3,73 @@ using DropOfAHat.Player;
 using UnityEngine;
 
 namespace DropOfAHat.Hat {
+    internal interface IHatState {
+        bool CanInteract => false;
+
+        IHatState Catch(GameObject caughtBy) => this;
+        IHatState Throw() => this;
+        IHatState LeaveObject(GameObject leaving) => this;
+        IHatState Pop() => this;
+        IHatState FinishPopping() => this;
+    }
+    internal class HatInAir : IHatState {
+        public bool CanInteract => true;
+
+        internal HatInAir() {}
+
+        public static IHatState Spawn() =>
+            new HatInAir();
+
+        public IHatState Catch(GameObject caughtBy) =>
+            new HatOnObject(caughtBy);
+
+        public IHatState Pop() =>
+            new HatPopping();
+    }
+
+    internal class HatPopping : IHatState {
+        internal HatPopping() {}
+
+        public IHatState FinishPopping() => 
+            new HatPopped();
+    }
+
+    internal class HatPopped : IHatState {
+        internal HatPopped() {}
+    }
+
+    internal class HatOnObject : IHatState {
+        public GameObject OnObject { get; }
+
+        internal HatOnObject(GameObject onObject) =>
+            OnObject = onObject;
+
+        public static IHatState SpawnOn(GameObject onObject) =>
+            new HatOnObject(onObject);
+
+        public IHatState Throw() =>
+            new HatLeavingObject(OnObject);
+    }
+
+    internal class HatLeavingObject : IHatState {
+        public bool CanInteract => true;
+
+        public GameObject LeavingObject { get; }
+
+        internal HatLeavingObject(GameObject leaving) =>
+            LeavingObject = leaving;
+
+        public IHatState LeaveObject(GameObject leaving) =>
+            leaving == LeavingObject
+            ? new HatInAir()
+            : this;
+
+        public IHatState Pop() =>
+            new HatPopping();
+    }
+    
     public class Hat : MonoBehaviour {
-        private const string IS_ON_PLAYER_ANIMATION_STATE = "IsOnPlayer";
+        private const string IS_ON_OBJECT_ANIMATION_STATE = "IsOnPlayer";
         private const string IS_POPPED_ANIMATION_STATE = "IsPopped";
         private const string VELOCITY_ANIMATION_STATE = "Velocity";
 
@@ -12,10 +77,7 @@ namespace DropOfAHat.Hat {
         private GameObject _player;
         [SerializeField]
         private Vector3 _offsetOnPlayer;
-        [SerializeField]
-        private bool _isOnPlayer = true;
 
-        private bool _inAir = false;
         private Rigidbody2D _rigidBody;
         private Collider2D _collider;
         private GameEvents _events;
@@ -23,71 +85,71 @@ namespace DropOfAHat.Hat {
         private ParticleSystem _particles;
         private AudioSource _hurtAudio;
 
+        private IHatState _hatState;
+
         private void Start() {
             _events = FindObjectOfType<GameEvents>();
             _rigidBody = GetComponent<Rigidbody2D>();
             _collider = GetComponent<Collider2D>();
             _animator = GetComponentInChildren<Animator>();
             _hurtAudio = GetComponent<AudioSource>();
-            if (_isOnPlayer) {
-                Catch();
-            }
+            Spawn();
             _events.Subscribe<PlayerThrow.HatThrown>(OnThrown);
-            _events.Subscribe<GameManager.LevelEndedEvent>(OnLevelEnd);
             _events.Subscribe<LevelStart.LevelLoadedEvent>(OnLevelLoad);
         }
 
         private void FixedUpdate() {
-            if (!_isOnPlayer) {
-                var velocity = _rigidBody.velocity;
-                var up = Vector3.up;
-                var angle = Vector3.Angle(up, velocity);
-                angle = velocity.x > 0
-                    ? -angle
-                    : angle;
-                transform.rotation =  Quaternion.Euler(0f, 0f, angle);
-                _animator.SetFloat(
-                    VELOCITY_ANIMATION_STATE, 
-                    velocity.magnitude);
+            if (_hatState is HatInAir || _hatState is HatLeavingObject) {
+                SetRotation();
             }
+        }
+
+        private void SetRotation() {
+            var velocity = _rigidBody.velocity;
+            var up = Vector3.up;
+            var angle = Vector3.Angle(up, velocity);
+            angle = velocity.x > 0
+                ? -angle
+                : angle;
+            transform.rotation = Quaternion.Euler(0f, 0f, angle);
+            _animator.SetFloat(
+                VELOCITY_ANIMATION_STATE,
+                velocity.magnitude);
         }
 
         private void Update() {
-            if (_isOnPlayer) {
-                transform.position = _player.transform.position + _offsetOnPlayer;
-                transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            if (_hatState is HatOnObject hat) {
+                transform.position = hat.OnObject.transform.position + _offsetOnPlayer;
+                transform.rotation = Quaternion.identity;
             }
         }
 
-        public void Catch() {
-            _inAir = false;
-            _isOnPlayer = true;
-            _animator.SetBool(IS_ON_PLAYER_ANIMATION_STATE, true);
+        public void Spawn() {
+            _hatState = HatOnObject.SpawnOn(_player);
+            Catch(_player);
+        }
+
+        public void Catch(GameObject caughtBy) {
+            _hatState = _hatState.Catch(caughtBy);
+            _animator.SetBool(IS_ON_OBJECT_ANIMATION_STATE, true);
             _animator.SetBool(IS_POPPED_ANIMATION_STATE, false);
             _animator.SetFloat(VELOCITY_ANIMATION_STATE, 0f);
-            SetPhysics(false);
-            _events.Send(new CaughtEvent());
+            SetPhysics(_hatState.CanInteract);
+            if (_hatState is HatOnObject) {
+                _events.Send(new CaughtEvent());
+            }
         }
 
         private void OnThrown(PlayerThrow.HatThrown thrown) {
-            if (_isOnPlayer) {
-                _isOnPlayer = false;
-                _animator.SetBool(IS_ON_PLAYER_ANIMATION_STATE, false);
-                SetPhysics(true);
-                _rigidBody.velocity = thrown.ThrowVec;
-            }
+            _hatState = _hatState.Throw();
+            _animator.SetBool(IS_ON_OBJECT_ANIMATION_STATE, false);
+            SetPhysics(_hatState.CanInteract);
+            Debug.Log(_hatState.CanInteract);
+            _rigidBody.velocity = thrown.ThrowVec;
         }
 
-        private void OnLevelLoad(LevelStart.LevelLoadedEvent _) {
-            _isOnPlayer = true;
-            Catch();
-        }
-
-        private void OnLevelEnd(GameManager.LevelEndedEvent _) {
-            if (!_isOnPlayer) {
-                Catch();
-            }
-        }
+        private void OnLevelLoad(LevelStart.LevelLoadedEvent _) =>
+            Spawn();
 
         private void SetPhysics(bool enabled) {
             _collider.enabled = enabled;
@@ -98,27 +160,26 @@ namespace DropOfAHat.Hat {
         }
         
         private void OnTriggerExit2D(Collider2D other) {
-            if (!_isOnPlayer && other.gameObject.CompareTag("Player")) {
-                _inAir = true;
-            }
+            _hatState = _hatState.LeaveObject(other.gameObject);
         }
 
         private void OnTriggerEnter2D(Collider2D other) {
-            if (_inAir) {
-                if (other.gameObject.CompareTag("Player")) {
-                    Catch();
-                }
-                if (other.gameObject.CompareTag("World") || other.gameObject.CompareTag("Enemy")) {
-                    Pop();
-                }
+            if (other.gameObject.CompareTag("Player")) {
+                Catch(other.gameObject);
+            }
+            if (other.gameObject.CompareTag("World") || other.gameObject.CompareTag("Enemy")) {
+                Pop();
             }
         }
 
         private void Pop() {
-            SetPhysics(false);
-            _animator.SetBool(IS_POPPED_ANIMATION_STATE, true);
-            _events.Send(new DroppedEvent());
-            _hurtAudio.Play();
+            _hatState = _hatState.Pop();
+            if (_hatState is HatPopping) {
+                SetPhysics(_hatState.CanInteract);
+                _animator.SetBool(IS_POPPED_ANIMATION_STATE, true);
+                _events.Send(new DroppedEvent());
+                _hurtAudio.Play();
+            }
         }
 
         public struct DroppedEvent { }
