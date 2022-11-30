b0261057b461765cfdd9549643ad5339d0bce4ee
Kendall Murphey <kendallmurphey@gmail.com>
2022-11-29 21:42:42 -0600

    rotated image

--- Assets/GameStart.cs
+++ Assets/GameStart.cs
@@ -12,6 +12,7 @@ public class GameStart : MonoBehaviour {
     [SerializeField]
     private Color _gameCoverColor;
 
+    private AudioSource _music;
     private GameTimer _timer;
     
     private void Awake() {
@@ -29,8 +30,9 @@ public class GameStart : MonoBehaviour {
     private IEnumerator LoadMenu() {
         yield return new WaitForSeconds(2f);
         //play the menu song
+
         _logo.enabled = true;
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(3f);
         Color c = _gameCover.material.color;
         for (float alpha = 1f; alpha >= 0f; alpha -= 0.02f)
         {
